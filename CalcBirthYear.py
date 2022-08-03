448f9539c8834abfc3309b125197ff6e02aa19fe
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-03 09:23:05 +0100

    Create CalcBirthYear.py

---
+++ CalcBirthYear.py
@@ -0,0 +1,14 @@
+# Task 2 - Program calculate year of birth
+
+
+# Calculate year of birth
+
+name = input("What is your name" + " ") # specify your name here
+age = int(input("age" + " ")) # age entered as int
+print("OMG, you are" + " " + str(age) + " " + "years old and was born in the year...")  # printing message with age/year.
+current_year = 2022
+year_born = (current_year - age)
+print(year_born)
+
+# Extra - go into library time to look how long person has lived?
+
